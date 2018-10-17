e4d5ca4a6afbf95f27c9606dce829871bafa345f
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-17 09:49:06 -0400

    adds the EFDbContext class and content

--- SportsStore.Domain/Concrete/EFDbContext.cs
+++ SportsStore.Domain/Concrete/EFDbContext.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SportsStore.Domain.Entities;
+using System.Data.Entity;
+
 namespace SportsStore.Domain.Concrete
 {
-    class EFDbContext
+    public class EFDbContext : System.Data.Entity.DbContext
     {
+        public DbSet<Product> Products { get; set; }
     }
 }
