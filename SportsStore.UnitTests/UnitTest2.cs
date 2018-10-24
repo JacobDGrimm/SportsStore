abd870f50125c80466ccce0fa78a9474ca23c16a
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-24 10:55:58 -0400

    adds a new unit test class to test the cart

---
+++ SportsStore.UnitTests/UnitTest2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Linq;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+
+        }
+    }
+}
