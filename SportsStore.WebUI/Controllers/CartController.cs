4b46cb54b5f41b9178d7528aeddfbf2151f6e5b1
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-25 13:48:35 -0400

    adds the cart controller and the appropriate using statements

---
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
